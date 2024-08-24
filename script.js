2b7c1259a67a8538a41bcbc21bc34e8a0a7dd34b
cosmosinstitute <144064768+cosmosinstitute@users.noreply.github.com>
2024-08-24 15:34:52 +0530

    kallu kaliya

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+function submitform(){
+    const formdata=new FormData(document.getElementById('profiledata'))
+    const name=formdata.get('name')
+    document.getElementById('hola').textContent='HELLO '+name
+    document.getElementById('profiledata').style.display='none'
+    for(const pair of formdata.entries()){
+        console.log(pair[0]+':'+pair[1])
+    }
+
+}
